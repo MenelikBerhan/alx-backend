b4dd82321e0f7fad664a0312a41e482c7a13d053
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-04 22:50:27 +0300

    Task 2

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,32 @@
+// connect to redis server, set and get values
+import { createClient, print } from 'redis';
+
+// on redis.v>4 should call client.connect()
+const client = createClient();
+// set event handlers for `error` and `connect` events
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
+client.on('connect', () => {
+  console.error('Redis client connected to the server');
+});
+
+// sets value to key schoolName & display result
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    print(err, reply);
+  });
+}
+
+// finds value associated with key `schoolName`
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) console.log(err.message);
+    else console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+displaySchoolValue('HolabertonSanFrancisco');
