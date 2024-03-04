d51ce3a86a8f98df2f88dae737d7271f47eedb59
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-04 22:31:48 +0300

    Task 1

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+// connect to redis server
+import { createClient } from 'redis';
+
+const client = createClient();
+// set event handlers for `error` and `connect` events
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
+client.on('connect', () => {
+  console.error('Redis client connected to the server');
+});
