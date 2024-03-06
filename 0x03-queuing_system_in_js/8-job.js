e140ad266258e00491b24a5aac1c82fe5d444288
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-07 00:46:20 +0300

    Task 10

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,24 @@
+// Writing the job creation function
+
+export default function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) throw new Error('Jobs is not an array');
+
+  for (const jobData of jobs) {
+    // create job of type 'push_notification_code_3' for each, and notify if no error encountered
+    const job = queue.create('push_notification_code_3', jobData)
+      .save((err) => {
+        if (!err) console.log(`Notification job created: ${job.id}`);
+      });
+
+    // add handlers for job `complete`, `progress` & `failed` events
+    job.on('complete', () => { console.log(`Notification job ${job.id} completed`); });
+
+    job.on('failed', (errorMessage) => {
+      console.error(`Notification job ${job.id} failed: ${errorMessage}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  }
+}
