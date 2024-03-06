3b863560a798c5fe2e9ae75624aeff8fc19cc545
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-06 23:08:00 +0300

    Add comments

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -4,10 +4,12 @@ import { createQueue } from 'kue';
 
 const queue = createQueue();
 
+// function to be used in processing jobs
 function sendNotification(phoneNumber, message) {
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
+// process jobs of type `'push_notification_code'`
 queue.process('push_notification_code', (job, done) => {
   sendNotification(job.data.phoneNumber, job.data.message);
   done();
